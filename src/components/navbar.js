6f94416040b52be8596f55cef69dc04e94a1be6e
Mimi <mariwammimi99@gmail.com>
2022-06-09 17:02:12 +0600

    cart done

---
+++ src/components/navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../styles/navbar.css";
+
+const Navbar = ({ setShow, size }) => {
+  return (
+    <nav>
+      <div className="nav_box">
+        <span className="my_shop" onClick={() => setShow(true)}>
+          Welcome to goriber Amazon
+        </span>
+        <div className="cart" onClick={() => setShow(false)}>
+          <span>
+            {" "}
+            Cart
+            <i className="fas fa-cart-plus"></i>
+          </span>
+          <span>{size}</span>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,36 @@
+const list = [
+  {
+    id: 1,
+    title: "oil",
+    author: "sheikh hasina",
+    price: 50000,
+    img: "https://m.media-amazon.com/images/I/71LcJ91B9BL._AC_UL320_.jpg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "mousepad",
+    author: "dokan",
+    price: 200,
+    img: "https://m.media-amazon.com/images/I/71I9assPTZL._AC_UL320_.jpg",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Mouse",
+    author: "dokan",
+    price: 500,
+    img: "https://m.media-amazon.com/images/I/61i0CV-tKpL._AC_UY218_.jpg",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "phone",
+    author: "dokan",
+    price: 90000,
+    img: "https://m.media-amazon.com/images/I/61U6oC65TTL._AC_UY218_.jpg",
+    amount: 1,
+  },
+];
+
+export default list;
